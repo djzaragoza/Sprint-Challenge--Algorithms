e2e90b1c62194a94142b05951a1f26fe1d185397
unknown <dj.zaragoza@icloud.com>
2020-06-13 10:24:47 -0700

    finished debugging and test complete ran successfully

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,9 +5,14 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 
 
-def count_th(word, count = 0):
-
+def count_th(word, count=0):
+    # Objective --
+    # recursive function checking the string for 'th' -- account for possible mixed cases
     # TBC
+    ## find every 'th' word only if letters 't' and 'h' are next to each other
+    
+    ## need to figure out a way to count words
+    ## need to figure out the account of mixed cases 
 
     if word[0:2] == 'th':
         count += 1
@@ -16,4 +21,5 @@ def count_th(word, count = 0):
         count = count_th(word[1:len(word)], count)
 
     return count
+
     pass
